63800094809bf6593e16c36c7987f177e100637b
Kornelijus Tvarijanavičius <16837066+Kornelijus@users.noreply.github.com>
2025-11-05 02:51:08 +0200

    package.py typing

--- scripts/package.py
+++ scripts/package.py
@@ -17,12 +17,18 @@ from const import (
 )
 
 
-def add_includes_to_package(package_file, includes, fonts, new_file, target):
+def add_includes_to_package(
+    package_file: str,
+    includes: list[str] | None,
+    fonts: list[str] | None,
+    new_file: str,
+    target: BuildTarget,
+):
     with tempfile.TemporaryDirectory() as temp_dir:
-        # Extract package
+        # Extract and remove package_file
         run(join(["7z", "x", package_file, f"-o{temp_dir}"]), exit_on_fail=False)
-        # Delete package_file
         os.remove(package_file)
+
         if package_file.endswith(".tar.xz"):
             # Rerun on the tar file
             package_tar = package_file[:-3]  # Remove ".xz"
@@ -127,7 +133,6 @@ def get_args():
 
 
 def main():
-    """The main packaging function"""
     args = get_args()
     file_ext = PACKAGE_FILE_EXTENSIONS[args.os]
 
